a48bb3868f378174e17389382a1d6a8220ec3e5e
Matt Smith <mattsmith@Matts-MacBook-Pro.local>
2024-09-27 17:12:25 -0400

    Update

--- sqh.js
+++ sqh.js
@@ -88,7 +88,7 @@ const deliveryAddress = 'Your Delivery Address Here'; // Replace with the actual
 
 	if (currentURL.includes("festool")) {
 	// Select the target div with the class 'row-breadcrumb'
-        const targetElement = document.querySelector('.productaddtocart_container')
+        const targetElement = document.querySelector('.productaddtocart_container');
 
 	    // Check if the target element exists
 	    if (targetElement) {
@@ -96,11 +96,20 @@ const deliveryAddress = 'Your Delivery Address Here'; // Replace with the actual
 		const newDiv = document.createElement('div');
 
 		// Set the content of the new div (innerHTML)
-		newDiv.innerHTML = "<a href='https://login.festool.com/Account/Login?ReturnUrl=%2Fconnect%2Fauthorize%2Fcallback%3Fscope%3Dcode%2520email%2520offline_access%2520openid%2520phone%2520profile%2520roles%2520terms%2520tts.cashback%2520tts.contact_request%2520tts.customer%2520tts.dealer%2520tts.lottery%2520tts.pdf_generator%2520tts.personal_data%2520tts.registered_machines%2520tts.repairorder%2520tts.sellout%2520tts.shipping_address%2520tts.shopping_list%2520tts.tan_generator%2520tts.warranty_request%2520tts.warrantyenabled_product%26state%3D%252Fmyfestool%26ui_locales%3Den-US%26client_id%3DcustomerPortal.us.tts.live%26response_type%3Dcode%2520id_token%26response_mode%3Dform_post%26nonce%3Df0a3afeb847f41aaa33c78e5549cba97%26redirect_uri%3Dhttps%253A%252F%252Fwww.festoolusa.com%252Fapi%252Fsitecore%252Fttsauthentication%252Flogin' target='_blank'><image style='margin: 0 auto'  src='https://matt-cloudengineering.github.io/sqh/register_sidebar.png' /></a>";
+		newDiv.innerHTML = "<a href='https://login.festool.com/Account/Login?ReturnUrl=%2Fconnect%2Fauthorize%2Fcallback%3Fscope%3Dcode%2520email%2520offline_access%2520openid%2520phone%2520profile%2520roles%2520terms%2520tts.cashback%2520tts.contact_request%2520tts.customer%2520tts.dealer%2520tts.lottery%2520tts.pdf_generator%2520tts.personal_data%2520tts.registered_machines%2520tts.repairorder%2520tts.sellout%2520tts.shipping_address%2520tts.shopping_list%2520tts.tan_generator%2520tts.warranty_request%2520tts.warrantyenabled_product%26state%3D%252Fmyfestool%26ui_locales%3Den-US%26client_id%3DcustomerPortal.us.tts.live%26response_type%3Dcode%2520id_token%26response_mode%3Dform_post%26nonce%3Df0a3afeb847f41aaa33c78e5549cba97%26redirect_uri%3Dhttps%253A%252F%252Fwww.festoolusa.com%252Fapi%252Fsitecore%252Fttsauthentication%252Flogin' target='_blank'><image style='display; block;margin: 10% auto'  src='https://matt-cloudengineering.github.io/sqh/register_sidebar.png' /></a>";
 
 		// Insert the new div after the 'row-breadcrumb' div
 		targetElement.insertAdjacentElement('afterend', newDiv);
 	    } else {
-		console.warn('Target element with class "row-breadcrumb" not found.');
+		const secondTargetElement = document.querySelector('.row-breadcrumb');	
+
+		// Create a new div element
+                const newDiv = document.createElement('div');
+
+                // Set the content of the new div (innerHTML)
+                newDiv.innerHTML = "<a href='https://login.festool.com/Account/Login?ReturnUrl=%2Fconnect%2Fauthorize%2Fcallback%3Fscope%3Dcode%2520email%2520offline_access%2520openid%2520phone%2520profile%2520roles%2520terms%2520tts.cashback%2520tts.contact_request%2520tts.customer%2520tts.dealer%2520tts.lottery%2520tts.pdf_generator%2520tts.personal_data%2520tts.registered_machines%2520tts.repairorder%2520tts.sellout%2520tts.shipping_address%2520tts.shopping_list%2520tts.tan_generator%2520tts.warranty_request%2520tts.warrantyenabled_product%26state%3D%252Fmyfestool%26ui_locales%3Den-US%26client_id%3DcustomerPortal.us.tts.live%26response_type%3Dcode%2520id_token%26response_mode%3Dform_post%26nonce%3Df0a3afeb847f41aaa33c78e5549cba97%26redirect_uri%3Dhttps%253A%252F%252Fwww.festoolusa.com%252Fapi%252Fsitecore%252Fttsauthentication%252Flogin' target='_blank'><image style='display; block;margin: 10% auto'  src='https://matt-cloudengineering.github.io/sqh/register_sidebar.png' /></a>";
+
+                // Insert the new div after the 'row-breadcrumb' div
+                secondTargetElement.insertAdjacentElement('afterend', newDiv);
 	    }
 	}
